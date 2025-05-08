68cccb132b9c1a2cd9fd1e3555cc270fb64a1dba
AbhishekR5 <abhishekramachandra98@gmail.com>
2025-05-08 17:07:49 +0530

    base model h5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cnnClassifier import logger
+import os
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
@@ -12,4 +13,22 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
+        raise e
+
+CONFIG_FILE_PATH = "path_to_yaml"
+
+def read_yaml(path_to_yaml):
+    """
+    Reads a YAML file and returns its content.
+    """
+    try:
+        logger.info(f"Attempting to read YAML file at: {os.path.abspath(path_to_yaml)}")
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return content
+    except FileNotFoundError as e:
+        logger.error(f"YAML file not found: {path_to_yaml}")
+        raise e
+    except Exception as e:
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -10,15 +10,17 @@ from cnnClassifier.entity.config_entity import (DataIngestionConfig,
 class ConfigurationManager:
     def __init__(
         self,
+        
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
-
+        CONFIG_FILE_PATH = "config/config.yaml"
+        PARAMS_FILE_PATH = "params.yaml"
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
-
+        
     
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
