5aa7252f9ad62364d1cb2586239910f857088ed1
AbhishekR5 <abhishekramachandra98@gmail.com>
2025-05-08 23:15:10 +0530

    updated evaluation method

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,7 +2,7 @@ import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,
-                                                PrepareCallbacksConfig, TrainingConfig)
+                                                PrepareCallbacksConfig, TrainingConfig, EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -93,17 +93,17 @@ class ConfigurationManager:
 
         return training_config
     
-"""
+
 
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Chicken-fecal-images",
-            mlflow_uri="https://dagshub.com/bappymalik4161/ML-Pipeline-using-DVC.mlflow",
+            #mlflow_uri="https://dagshub.com/bappymalik4161/ML-Pipeline-using-DVC.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
         )
         return eval_config
 
-      """
+      
